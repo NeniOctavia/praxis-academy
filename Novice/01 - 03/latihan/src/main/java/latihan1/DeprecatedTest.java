3b1a3b3b838e90dcb030fca911b1cf9053b8e1d1
NeniOctavia <neni1700016073@webmail.uad.ac.id>
2020-03-07 01:30:53 +0700

    baru

---
+++ Novice/01 - 03/latihan/src/main/java/latihan1/DeprecatedTest.java
@@ -0,0 +1,15 @@
+package latihan1;
+
+public class DeprecatedTest
+{
+    @Deprecated
+    public void Display()
+    {
+        System.out.println("Deprecatedtest display()");
+    }
+   public static void main(String args[])
+    {
+        DeprecatedTest d1 = new DeprecatedTest();
+        d1.Display();
+    }
+}
